a6b8744eecfe113707148f984f69f99cce05f591
francisois <francisois@9241ba81-fa4c-0410-94a8-2bda7a3c0abc>
2008-05-11 20:48:41 +0000

    first commit

---
+++ trunk/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  DebMaker
+//
+//  Created by francis on 5/11/08.
+//  Copyright iLiberty Team 2008. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
